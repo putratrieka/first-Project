327efb71848e21037543109d8c6f525e68a24e2e
putratrieka <44422431+putratrieka@users.noreply.github.com>
2018-10-29 23:29:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,299 @@
+<?php 
+	require_once 'core/init.php';
+	include 'includes/head.php';
+ 	include 'includes/navigation.php';
+ 	include 'includes/slider-nav-menu.php';
+ 	include 'includes/site-nav-menu-dropdown.php';
+ ?>
+
+<!--====Hot Deals -Box===-->
+		<div class="hot-deals-box">
+			<div class="title-rows">
+				<div class="box-left"><h4>HOT DEALS</h4></div>
+				<div class="box-right"><a href=""><button class="btn btn-primary" style="color: blue; background:#f8f8f8 ; border-radius: 1px;font-family: sans-serif;">HOT DEALS</button></a></div>
+			</div>
+			<div class="hot-deals-container">
+				<a href="#">
+					<div class="hot-deals-product">
+						<div class="product-image">
+							<img src="images/hot-deals1.jpg">
+						</div>
+						<div class="product-title">
+							<a href="#">Hot Deals - Seagate Backup Plus Slim Hard Disk Eksternal - Silver</a>
+						</div>
+						<div class="product-price">
+							<div class="old-price">
+								<span class="old-price-text">Rp5,235,000</span>
+							</div>
+							<div class="new-price">
+								<span class="new-price-text">Rp4,599,000</span>
+								<div class="discount">
+									<span>12% OFF</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="hot-deals-product">
+						<div class="product-image">
+							<img src="images/hot-deals1.jpg">
+						</div>
+						<div class="product-title">
+							<a href="#">Hot Deals - Seagate Backup Plus Slim Hard Disk Eksternal - Silver</a>
+						</div>
+						<div class="product-price">
+							<div class="old-price">
+								<span class="old-price-text">Rp5,235,000</span>
+							</div>
+							<div class="new-price">
+								<span class="new-price-text">Rp4,599,000</span>
+								<div class="discount">
+									<span>12% OFF</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="hot-deals-product">
+						<div class="product-image">
+							<img src="images/hot-deals1.jpg">
+						</div>
+						<div class="product-title">
+							<a href="#">Hot Deals - Seagate Backup Plus Slim Hard Disk Eksternal - Silver</a>
+						</div>
+						<div class="product-price">
+							<div class="old-price">
+								<span class="old-price-text">Rp5,235,000</span>
+							</div>
+							<div class="new-price">
+								<span class="new-price-text">Rp4,599,000</span>
+								<div class="discount">
+									<span>12% OFF</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="hot-deals-product">
+						<div class="product-image">
+							<img src="images/hot-deals1.jpg">
+						</div>
+						<div class="product-title">
+							<a href="#">Hot Deals - Seagate Backup Plus Slim Hard Disk Eksternal - Silver</a>
+						</div>
+						<div class="product-price">
+							<div class="old-price">
+								<span class="old-price-text">Rp5,235,000</span>
+							</div>
+							<div class="new-price">
+								<span class="new-price-text">Rp4,599,000</span>
+								<div class="discount">
+									<span>12% OFF</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+											
+			</div>
+			<div class="dropdown-divider"></div>
+			<a href="#">Syarat dan ketetntuan</a>
+		</div>
+<!--====Product-Box===-->	
+		<div class="product-box">
+			<div class="title-rows">
+				<div class="box-left"><h4 style="color: #ccb00e;border-left: 10px solid #ccb00e;">PRODUCT</h4></div>
+				<div class="box-right"><a href=""><button class="btn btn-primary" style="color: blue; background:#f8f8f8 ; border-radius: 1px;font-family: sans-serif;">View All</button></a></div>
+			</div>
+			<div class="product-container owl-carousel owl-theme">
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/men4.png" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/men3.png" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+				
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/men1.png" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+				
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/men2.png" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+				
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/men3.png" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/men5.png" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/men6.png" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/men7.png" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/shoes.jpg" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/women1.png" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/women2.png" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+
+				<div class="prod-img product-image item">
+					<h4>Levis Jeans</h4>
+					<img src="images/prod-img/women3.png" alt="Levis Jeans" class="img-thumb">
+					<div class="old-price">
+						<span class="old-price-text">Rp5,235,000</span>
+					</div>
+					<div class="new-price">
+						<span class="new-price-text">Rp4,599,000</span>
+						<div class="discount">
+							<span>12% OFF</span>
+						</div>
+					</div>
+					<button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#details-1">Details</button>
+				</div>
+
+			</div>
+		</div>
+<!--====Footer====-->
+</div>	<!-- END OF BODY AREA-->
+
+
+<?php 
+	include 'includes/detailsmodal.php';
+ 	include 'includes/footer.php';
+ ?>
---
+++ register.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>::Registration Form::</title>
+	<link rel="stylesheet" type="text/css" href="fa/css/all.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery.bxslider.min.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="wrapper">
+		<div class="nav-wrapper-container">
+			<div class="container-navbar">
+				<div class="container-navbar-left">
+					<ul>
+						<li><a href="#">Jual</a></li>
+						<li class="with-border"><a href="#">Download</a></li>
+						<li><a href="#">Kunjungi kami di</a></li>
+						<li><a href="#" title="Kunjungi kami di line"><i class="fab fa-line"></i></a></li>
+						<li><a href="#" title="Kunjungi kami di facebook"><i class="fab fa-facebook"></i></a></li>
+						<li><a href="https://www.instagram.com/triekaputra/" title="Kunjungi kami di instaram"><i class="fab fa-instagram"></i></a></li>
+					</ul>					
+				</div>
+				<div class="container-navbar-right">
+					<ul>
+						<li><a href="#">Notifikasi <i class="far fa-bell"></i></a></li>
+						<li><a href="#">Bantuan <i class="far fa-question-circle"></i></a></li>
+					</ul>						
+				</div>
+			<!--end of container-navbar-->
+			</div>
+		<!-- end of nav-wrapper-container-->
+		</div>
+		<div class="navigation-bar">
+			<div class="container">
+				<div class="logo">
+					<a href="index.php"><img src="images/TEP-Logo.png"></a>
+				</div>	
+			<div class="search-area">
+				<input class="search-box" type="text" name="" placeholder="Cari produk, merek dan toko">
+				<button class="search_btn btn btn-white"><i class="fas fa-search"></i></button>
+				<div class="hot-words">
+					<li><a href="#">Sepatu pria</a></li>
+					<li><a href="#">Sepatu wanita</a></li>
+					<li><a href="#">Fashion pria</a></li>
+					<li><a href="#">Fahion wanita</a></li>
+					<li><a href="#">Sepatu futsal</a></li>	
+					<li><a href="#">Pakaian muslim</a></li>						
+				</div>		
+			</div>
+			<div class="user-menu">
+				<ul>
+					<li><a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-hoshpopup="true" aria-expaded="false"><i class="fas fa-user"></i> My Account</a>
+						<!--===Dropdown-Box===-->
+						<div class="dropdown-menu" dropdown-menu-right bg-dark" style="width: 250px;">
+							<a href="#"><button type="button" class="dropdown-item"><i class="fas fa-user"></i> Your Account</button></a>
+							<a href="#"><button type="button" class="dropdown-item"><i class="fas fa-cube"></i> Your order</button></a>
+							<a href="#"><button type="button" class="dropdown-item"><i class="fas fa-heart"></i> Wishlist</button></a>
+							<div class="dropdown-divider"></div>
+							<p class="text-center text-white" style="height: 15px; line-height: 2px;"><small>if you are a new user</small></p>
+							<a href="register.php"><button type="button" class="dropdown-item text-center"><i class="fas fa-user"></i> Register</button></a>
+							<a href="#"><button type="button" class="dropdown-item text-center" style="background: #cc0e4e;"><i class="fas fa-user"></i> LOGIN</button></a>
+						</div>
+					</li>
+					<li><a href="#"><i class="fas fa-shopping-cart"></i> Cart</a></li>
+				</ul>
+				
+			</div>
+			<!-- end of container-->
+			</div>
+		<!-- end of navigation-bar-->
+		</div>
+	</div>
+	</header>
+
+<div class="body-area">
+		<div class="registration_header">
+			<h3>Buat Akun TepID anda</h3>
+			<div  class="login-other">
+				<span>Sudah menjadi member?</span><a href=""> login </a>disini</span>
+			</div>
+		</div>
+		<div class="registration_form">
+			<form>
+				<div class="registration_form_col1">
+					<div class="reg-input-phone-num">
+						<label>Nomor Telepon</label>
+						<input type="text" name="" placeholder="Masukkan nomor telepon Anda">
+						<label></label>
+						<span></span>
+					</div>
+					<div class="reg-input-sms">
+						<label>SMS Kode Verivikasi</label>
+						<input type="text" name="" placeholder="6 digits">
+						<label></label>
+						<span></span>
+					</div>
+					<div class="reg-input-sandi">
+						<label>Minimal 6 Karakter terdiridari angka dan huruf</label>
+						<input type="text" name="" placeholder="Minimal 6 Karakter terdiridari angka dan huruf">
+						<label></label>
+						<span></span>
+					</div>
+					<div class="reg-login-birthday">
+						<div class="reg-login-birthday-hd" style="font-size: 12px; color: black">Tanggal lahir</div>
+						<div class="reg-birthday">
+							
+						</div>
+						
+					</div>
+				</div>
+				<div class="registration_form_col2">
+					<div class="reg-input-name">
+						<label>Nama lengkap</label>
+						<input type="text" name="" placeholder="Nama lengkap">
+						<label></label>
+						<span></span>
+					</div>
+					<div class="reg-input-email">
+						<label>Alamat Email</label>
+						<input type="text" name="" placeholder="Mohon isi alamat email Anda">
+						<label></label>
+						<span></span>
+					</div>
+					<div class="login-receive">
+						<label class="next-checkbox chacked ">
+							<span>
+								<i class="next-icon next-icon-select next-icon-xs"></i>
+							</span>
+						</label>
+						<p style="font-size: 12px; color: gray"> Saya ingin mendapatkan penawaran ekslusif dan promosi dari tepID</p>
+						
+						<label></label>
+						<span></span>
+					</div>
+					<div class="reg-btn">
+						<button class="reg_btn btn btn-primary" type="submit" style="background: #ccb00e; width: 280px; margin: auto;">DAFTAR</button>
+					</div>
+				</div>
+			</form>
+		</div>
+		
+	<!-- END OF BODY AREA-->
+</div>
+
+
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.bxslider.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/bootstrap.bundle.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/all.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
